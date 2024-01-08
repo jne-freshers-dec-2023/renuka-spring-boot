30d59667fc17970a19cc80a357c6d8e48dcab7e2
renukaThink <renuka.pogul@thinkitive.com>
2024-01-08 18:23:51 +0530

    Profile configuration

--- learnspringboot/src/main/java/com/springday2/learnspringboot/day1/profileproperties/ProfilePropertiesConfiguration.java
+++ learnspringboot/src/main/java/com/springday2/learnspringboot/day1/profileproperties/ProfilePropertiesConfiguration.java
@@ -1,5 +1,6 @@
 package com.springday2.learnspringboot.day1.profileproperties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -7,10 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "profileproperties")
 public class ProfilePropertiesConfiguration {
-    private  String url;
+    @Value("${profileproperties.url}")
+    public static   String url;
     private  String username;
     private String key;
     public String getUrl() {
+        System.out.println(url);
         return url;
     }
     public void setUrl(String url) {
